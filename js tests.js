2bf509407eb47cd3814218ec21a16c75444fdfce
danny-kenn <davidkiamba336@gmail.com>
2024-05-07 16:43:08 +0300

    modified the system

---
+++ js tests.js
@@ -0,0 +1,9 @@
+
+    var myAge =25;
+    if(myAge >= 18 && myAge <= 35){
+
+        document.write("You can come, you cool dude");
+    } else{
+
+        document.write("Youre underage not elegible, sorry");
+    }
