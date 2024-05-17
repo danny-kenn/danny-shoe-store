451d8e4f202638559cd96853ad2e7a1ea7148b7d
danny-kenn <davidkiamba336@gmail.com>
2024-05-17 12:28:24 +0300

    adjusted js to cache

--- js tests.js
+++
@@ -1,9 +0,0 @@
-
-    var myAge =25;
-    if(myAge >= 18 && myAge <= 35){
-
-        document.write("You can come, you cool dude");
-    } else{
-
-        document.write("Youre underage not elegible, sorry");
-    }
